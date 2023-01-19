f97e14baca3b144a3623b8cb16925a1998d0828c
eduardo-solis <solishernandezeduardosolis@gmail.com>
2023-01-19 13:07:19 -0600

    Agregando las rutas iniciales

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import Enrutador from "./components/Enrutador"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <h1>Bienvenido</h1>
-     
-    </div>
+    <>
+    <Enrutador></Enrutador>
+    </>
   )
 }
 
---
+++ src/components/Enrutador.jsx
@@ -0,0 +1,38 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+const Enrutador = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                {/* Vista general */}
+                <Route path='/' element={ <><h1>Inico</h1></> } />
+
+                {/* Ruta para el inicio de sesion */}
+                <Route path='/iniciar_sesion' element={ <><h1>Login</h1></> } />
+                <Route path='/registrar' element={ <><h1>Regitrar cliente</h1></> } />
+
+                {/* Rutas administrativas */}
+                <Route path='/admin/*' >
+                    <Route path='cursos' element={ <><h1>Cursos</h1></> } />
+                    <Route path='lecciones' element={ <><h1>Lecciones</h1></> } />
+                    <Route path='cuestionarios' element={ <><h1>Cuestionarios</h1></> } />
+                    <Route path='preguntas' element={ <><h1>Preguntas</h1></> } />
+                    <Route path='empleados' element={ <><h1>Empleados</h1></> } />
+                    <Route path='usuarios' element={ <><h1>Usuarios</h1></> } />
+                </Route>
+
+                {/* Rutas del cliente */}
+                <Route path='/cliente/*' >
+                    <Route path='mis_cursos' element={ <><h1>Cursos (Cliente)</h1></> } />
+                    <Route path='perfil' element={ <><h1>Perfil</h1></> } />
+                    <Route path='comprar' element={ <><h1>Comprar</h1></> } />
+                    <Route path='pagar' element={ <><h1>Pagar</h1></> } />
+                    <Route path='pagar' element={ <><h1>Pagar</h1></> } />
+                </Route>
+
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default Enrutador
