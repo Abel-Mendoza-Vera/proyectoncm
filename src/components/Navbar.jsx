a65345829913d36177bcc7530afa0331372ffac6
eduardo-solis <solishernandezeduardosolis@gmail.com>
2023-01-21 09:43:16 -0600

    Modificando el navegador

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,10 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/" >
+                <Link to="/" className="navbar-brand" >
                     <img src={logo} alt="Logo de Novatec Consultores Mexico" height='35' />
-                </a>
+                </Link>
+                
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
