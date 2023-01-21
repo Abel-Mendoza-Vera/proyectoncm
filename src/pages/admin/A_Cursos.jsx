1bf3381ae1451ce596a700f5b498d77a76544b28
eduardo-solis <solishernandezeduardosolis@gmail.com>
2023-01-21 10:10:21 -0600

    Agregando la primera fila

--- src/pages/admin/A_Cursos.jsx
+++ src/pages/admin/A_Cursos.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 const A_Cursos = () => {
   return (
-    <div>A_Cursos</div>
+    <>
+    <div className='container-fluid mt-3'>
+
+      {/** Primer fila para agregar o buscar un curso */}
+      <div className="row">
+        <div className="col">
+          <button className='btn btn-outline-success' ><span className="material-icons">add</span>Agregar</button>
+        </div>
+
+        <div className='col'>
+          <div className="input-group">
+            <input className='form-control' placeholder='Buscar' type="search" name="buscar" />
+            <span className='input-group-text material-icons'>search</span>
+          </div>
+        </div>
+      </div>
+
+      
+
+    </div>
+    </>
   )
 }
 
