311c9aa3229732f78fe2a4431cc73e5f50cc7f29
Abel-Mendoza-Vera <Abelescorpion2@gmail.com>
2023-01-21 11:49:16 -0600

    Cursos

--- src/pages/admin/A_Cursos.jsx
+++ src/pages/admin/A_Cursos.jsx
@@ -20,10 +20,11 @@ const A_Cursos = () => {
         </div>
 
         { /** Lista de las tarjetas de los cursos */}
-        <div className='mt-3 d-flex flex-wrap' >
+        <div className='row mt-5 g-4' >
 
         { /** Tarjetas del cursos */}
-          <div className="card m-3" style={{ width: "18rem" }}>
+        <div className='col'>
+          <div className="card" style={{ width: "18rem" }}>
             <img src="https://www.lucaedu.com/wp-content/uploads/2021/11/Que-es-un-curso-en-linea.jpg" className="card-img-top" alt="..." />
             <div className="card-body">
               <h5 className="card-title">Nombre del curso</h5>
@@ -36,10 +37,31 @@ const A_Cursos = () => {
                 <button className='btn btn-danger'><span className='material-icons'>delete</span></button>
               </div>
             </div>
+          </div>
+          </div>
+          
+
+          { /** Tarjetas del cursos */}
+          <div className='col'>
+          <div className="card" style={{ width: "18rem" }}>
+            <img src="https://www.lucaedu.com/wp-content/uploads/2021/11/Que-es-un-curso-en-linea.jpg" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Nombre del curso</h5>
+              <p className="card-text">Descripción del curso</p>
+            </div>
+            <div className="card-footer">
+              <div className="d-flex justify-content-end">
+                <button className='btn btn-primary'><span className='material-icons'>edit</span></button>
+                <div className="mx-1"></div>
+                <button className='btn btn-danger'><span className='material-icons'>delete</span></button>
+              </div>
+            </div>
+          </div>
           </div>
 
           { /** Tarjetas del cursos */}
-          <div className="card m-3" style={{ width: "18rem" }}>
+          <div className='col'>
+          <div className="card" style={{ width: "18rem" }}>
             <img src="https://www.lucaedu.com/wp-content/uploads/2021/11/Que-es-un-curso-en-linea.jpg" className="card-img-top" alt="..." />
             <div className="card-body">
               <h5 className="card-title">Nombre del curso</h5>
@@ -52,10 +74,12 @@ const A_Cursos = () => {
                 <button className='btn btn-danger'><span className='material-icons'>delete</span></button>
               </div>
             </div>
+          </div>
           </div>
 
           { /** Tarjetas del cursos */}
-          <div className="card m-3" style={{ width: "18rem" }}>
+          <div className='col'>
+          <div className="card" style={{ width: "18rem" }}>
             <img src="https://www.lucaedu.com/wp-content/uploads/2021/11/Que-es-un-curso-en-linea.jpg" className="card-img-top" alt="..." />
             <div className="card-body">
               <h5 className="card-title">Nombre del curso</h5>
@@ -68,10 +92,12 @@ const A_Cursos = () => {
                 <button className='btn btn-danger'><span className='material-icons'>delete</span></button>
               </div>
             </div>
+          </div>
           </div>
 
           { /** Tarjetas del cursos */}
-          <div className="card m-3" style={{ width: "18rem" }}>
+          <div className='col'>
+          <div className="card" style={{ width: "18rem" }}>
             <img src="https://www.lucaedu.com/wp-content/uploads/2021/11/Que-es-un-curso-en-linea.jpg" className="card-img-top" alt="..." />
             <div className="card-body">
               <h5 className="card-title">Nombre del curso</h5>
@@ -84,10 +110,31 @@ const A_Cursos = () => {
                 <button className='btn btn-danger'><span className='material-icons'>delete</span></button>
               </div>
             </div>
+          </div>
+          </div>
+
+
+          { /** Tarjetas del cursos */}
+          <div className='col'>
+          <div className="card" style={{ width: "18rem" }}>
+            <img src="https://www.lucaedu.com/wp-content/uploads/2021/11/Que-es-un-curso-en-linea.jpg" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Nombre del curso</h5>
+              <p className="card-text">Descripción del curso</p>
+            </div>
+            <div className="card-footer">
+              <div className="d-flex justify-content-end">
+                <button className='btn btn-primary'><span className='material-icons'>edit</span></button>
+                <div className="mx-1"></div>
+                <button className='btn btn-danger'><span className='material-icons'>delete</span></button>
+              </div>
+            </div>
+          </div>
           </div>
 
           { /** Tarjetas del cursos */}
-          <div className="card m-3" style={{ width: "18rem" }}>
+          <div className='col'>
+          <div className="card" style={{ width: "18rem" }}>
             <img src="https://www.lucaedu.com/wp-content/uploads/2021/11/Que-es-un-curso-en-linea.jpg" className="card-img-top" alt="..." />
             <div className="card-body">
               <h5 className="card-title">Nombre del curso</h5>
@@ -101,6 +148,28 @@ const A_Cursos = () => {
               </div>
             </div>
           </div>
+          </div>
+
+
+          { /** Tarjetas del cursos */}
+          <div className='col'>
+          <div className="card" style={{ width: "18rem" }}>
+            <img src="https://www.lucaedu.com/wp-content/uploads/2021/11/Que-es-un-curso-en-linea.jpg" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Nombre del curso</h5>
+              <p className="card-text">Descripción del curso</p>
+            </div>
+            <div className="card-footer">
+              <div className="d-flex justify-content-end">
+                <button className='btn btn-primary'><span className='material-icons'>edit</span></button>
+                <div className="mx-1"></div>
+                <button className='btn btn-danger'><span className='material-icons'>delete</span></button>
+              </div>
+            </div>
+          </div>
+          </div>
+
+
 
         </div>
 
