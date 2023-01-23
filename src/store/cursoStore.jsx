bec91b8f2d64b005a03da5add2ba4fb9d7089771
eduardo-solis <solishernandezeduardosolis@gmail.com>
2023-01-23 00:35:38 -0600

    Agregando el gestor global, Cambiando la vista del curso (Administrativa)

---
+++ src/store/cursoStore.jsx
@@ -0,0 +1,128 @@
+import { create } from "zustand";
+
+export const useCursoStore = create(() => ({
+    cursos: [
+        {
+            idCurso: 1,
+            nombre: "Liderazgo personal",
+            precio: 550,
+            estatus: 1,
+            descripcion: "En este curso aprenderas las bases para poder gobernarte a ti mismo, para que lleges a ser una persona mas decisiva en la vida",
+            objetivos: "Crecer personalmente",
+            duracion: 5,
+            idVideo: 0,
+            idMiniatura: 0
+        },
+        {
+            idCurso: 2,
+            nombre: "Liderazgo personal",
+            precio: 550,
+            estatus: 0,
+            descripcion: "En este curso aprenderas las bases para poder gobernarte a ti mismo, para que lleges a ser una persona mas decisiva en la vida",
+            objetivos: "Crecer personalmente",
+            duracion: 5,
+            idVideo: 0,
+            idMiniatura: 0
+        },
+        {
+            idCurso: 3,
+            nombre: "Liderazgo personal",
+            precio: 550,
+            estatus: 1,
+            descripcion: "En este curso aprenderas las bases para poder gobernarte a ti mismo, para que lleges a ser una persona mas decisiva en la vida",
+            objetivos: "Crecer personalmente",
+            duracion: 5,
+            idVideo: 0,
+            idMiniatura: 0
+        },
+        {
+            idCurso: 4,
+            nombre: "Liderazgo personal",
+            precio: 550,
+            estatus: 1,
+            descripcion: "En este curso aprenderas las bases para poder gobernarte a ti mismo, para que lleges a ser una persona mas decisiva en la vida",
+            objetivos: "Crecer personalmente",
+            duracion: 5,
+            idVideo: 0,
+            idMiniatura: 0
+        },
+        {
+            idCurso: 5,
+            nombre: "Liderazgo personal",
+            precio: 550,
+            estatus: 1,
+            descripcion: "En este curso aprenderas las bases para poder gobernarte a ti mismo, para que lleges a ser una persona mas decisiva en la vida",
+            objetivos: "Crecer personalmente",
+            duracion: 5,
+            idVideo: 0,
+            idMiniatura: 0
+        },
+        {
+            idCurso: 6,
+            nombre: "Liderazgo personal",
+            precio: 550,
+            estatus: 1,
+            descripcion: "En este curso aprenderas las bases para poder gobernarte a ti mismo, para que lleges a ser una persona mas decisiva en la vida",
+            objetivos: "Crecer personalmente",
+            duracion: 5,
+            idVideo: 0,
+            idMiniatura: 0
+        },
+        {
+            idCurso: 7,
+            nombre: "Liderazgo personal",
+            precio: 550,
+            estatus: 1,
+            descripcion: "En este curso aprenderas las bases para poder gobernarte a ti mismo, para que lleges a ser una persona mas decisiva en la vida",
+            objetivos: "Crecer personalmente",
+            duracion: 5,
+            idVideo: 0,
+            idMiniatura: 0
+        },
+        {
+            idCurso: 8,
+            nombre: "Liderazgo personal",
+            precio: 550,
+            estatus: 1,
+            descripcion: "En este curso aprenderas las bases para poder gobernarte a ti mismo, para que lleges a ser una persona mas decisiva en la vida",
+            objetivos: "Crecer personalmente",
+            duracion: 5,
+            idVideo: 0,
+            idMiniatura: 0
+        },
+        {
+            idCurso: 9,
+            nombre: "Liderazgo personal",
+            precio: 550,
+            estatus: 1,
+            descripcion: "En este curso aprenderas las bases para poder gobernarte a ti mismo, para que lleges a ser una persona mas decisiva en la vida",
+            objetivos: "Crecer personalmente",
+            duracion: 5,
+            idVideo: 0,
+            idMiniatura: 0
+        },
+        {
+            idCurso: 10,
+            nombre: "Liderazgo personal",
+            precio: 550,
+            estatus: 1,
+            descripcion: "En este curso aprenderas las bases para poder gobernarte a ti mismo, para que lleges a ser una persona mas decisiva en la vida",
+            objetivos: "Crecer personalmente",
+            duracion: 5,
+            idVideo: 0,
+            idMiniatura: 0
+        },
+        {
+            idCurso: 11,
+            nombre: "Liderazgo personal",
+            precio: 550,
+            estatus: 1,
+            descripcion: "En este curso aprenderas las bases para poder gobernarte a ti mismo, para que lleges a ser una persona mas decisiva en la vida",
+            objetivos: "Crecer personalmente",
+            duracion: 5,
+            idVideo: 0,
+            idMiniatura: 0
+        },
+        
+    ]
+}))
