02b6fdb4b736f02b3513e6fe79a0eb480b878d2c
Abel-Mendoza-Vera <Abelescorpion2@gmail.com>
2023-02-26 15:39:47 -0600

    Certificaciones

--- src/components/Certificaciones.jsx
+++ src/components/Certificaciones.jsx
@@ -28,23 +28,67 @@ const Certificaciones = () => {
                         <span className='input-group-text material-icons'>search</span>
                     </div>
                 </div>
-                <ul class="nav nav-tabs">
-                    <li class="nav-item">
-                        <a class="nav-link" onClick={Cursos}>Carrito</a>
+                <ul className="nav nav-tabs">
+                    <li className="nav-item">
+                        <a className="nav-link" onClick={Cursos}>Carrito</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" onClick={MisCursos}>Mis Cursos</a>
+                    <li className="nav-item">
+                        <a className="nav-link" onClick={MisCursos}>Mis Cursos</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" onClick={Certificaciones}>Cursos Terminados</a>
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" onClick={Certificaciones}>Cursos Terminados</a>
                     </li>
                 </ul>
             </div>
 
-            <div>
-                Hola
+            <div className="text-center">
+                <h4>
+                    Mis Certificaciones
+                </h4>
             </div>
 
+            <section className="h-100" style={{ backgroundcolor: "#eee" }}>
+                <div className="container h-100 py-5">
+                    <div className="row d-flex justify-content-center align-items-center h-100">
+                        <div className="col-10">
+
+                            <div className="card rounded-3 mb-4">
+                                <div className="card-body p-4">
+                                    <div className="row d-flex justify-content-between align-items-center">
+                                        <div className="col-md-2 col-lg-2 col-xl-2">
+                                            <img
+                                                src={img}
+                                                className="img-fluid rounded-3" alt="Cotton T-shirt" />
+                                        </div>
+                                        <div className="col-md-3 col-lg-3 col-xl-3">
+                                            <p className="lead fw-normal mb-2">Curso 1</p>
+                                        </div>
+                                        <div className="col-md-3 col-lg-3 col-xl-2 d-flex">
+                                            <button className="btn btn-link px-2"
+                                                onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
+                                                <i className="fas fa-minus"></i>
+                                            </button>
+
+
+                                            <button className="btn btn-link px-2"
+                                                onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
+                                                <i className="fas fa-plus"></i>
+                                            </button>
+                                        </div>
+                                        <div className="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
+                                        <button type="button" className="btn btn-success btn-block">Descargar</button>
+                                        </div>
+                                        <div className="col-md-1 col-lg-1 col-xl-1 text-end">
+                                            <a href="#!" className="text-danger"><i className="fas fa-trash fa-lg"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
--- src/components/Cursos.jsx
+++ src/components/Cursos.jsx
@@ -27,170 +27,169 @@ const Cursos = () => {
                         <span className='input-group-text material-icons'>search</span>
                     </div>
                 </div>
-                <ul class="nav nav-tabs">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" onClick={Cursos}>Carrito</a>
+                <ul className="nav nav-tabs">
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" onClick={Cursos}>Carrito</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" onClick={MisCursos}>Mis Cursos</a>
+                    <li className="nav-item">
+                        <a className="nav-link" onClick={MisCursos}>Mis Cursos</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" onClick={Certificaciones}>Cursos Terminados</a>
+                    <li className="nav-item">
+                        <a className="nav-link" onClick={Certificaciones}>Cursos Terminados</a>
                     </li>
                 </ul>
             </div>
 
 
 
-            <section class="h-100" style={{ backgroundcolor: "#eee" }}>
-                <div class="container h-100 py-5">
-                    <div class="row d-flex justify-content-center align-items-center h-100">
-                        <div class="col-10">
+            <section className="h-100" style={{ backgroundcolor: "#eee" }}>
+                <div className="container h-100 py-5">
+                    <div className="row d-flex justify-content-center align-items-center h-100">
+                        <div className="col-10">
 
-                            <div class="d-flex justify-content-between align-items-center mb-4">
-                                <h3 class="fw-normal mb-0 text-black">Carrito de Compras</h3>
+                            <div className="d-flex justify-content-between align-items-center mb-4">
+                                <h3 className="fw-normal mb-0 text-black">Carrito de Compras</h3>
                             </div>
 
-                            <div class="card rounded-3 mb-4">
-                                <div class="card-body p-4">
-                                    <div class="row d-flex justify-content-between align-items-center">
-                                        <div class="col-md-2 col-lg-2 col-xl-2">
+                            <div className="card rounded-3 mb-4">
+                                <div className="card-body p-4">
+                                    <div className="row d-flex justify-content-between align-items-center">
+                                        <div className="col-md-2 col-lg-2 col-xl-2">
                                             <img
                                                 src={img}
-                                                class="img-fluid rounded-3" alt="Cotton T-shirt" />
+                                                className="img-fluid rounded-3" alt="Cotton T-shirt" />
                                         </div>
-                                        <div class="col-md-3 col-lg-3 col-xl-3">
-                                            <p class="lead fw-normal mb-2">Curso 1</p>
-                                            <p><span class="text-muted">Duración: </span>18 </p>
+                                        <div className="col-md-3 col-lg-3 col-xl-3">
+                                            <p className="lead fw-normal mb-2">Curso 1</p>
+                                            <p><span className="text-muted">Duración: </span>18 </p>
                                         </div>
-                                        <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
-                                            <button class="btn btn-link px-2"
+                                        <div className="col-md-3 col-lg-3 col-xl-2 d-flex">
+                                            <button className="btn btn-link px-2"
                                                 onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
-                                                <i class="fas fa-minus"></i>
+                                                <i className="fas fa-minus"></i>
                                             </button>
 
                                             <input id="form1" min="0" name="quantity" value="1" type="number"
-                                                class="form-control form-control-sm" />
+                                                className="form-control form-control-sm" />
 
-                                            <button class="btn btn-link px-2"
+                                            <button className="btn btn-link px-2"
                                                 onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
-                                                <i class="fas fa-plus"></i>
+                                                <i className="fas fa-plus"></i>
                                             </button>
                                         </div>
-                                        <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                                            <h5 class="mb-0">$499.00</h5>
+                                        <div className="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
+                                            <h5 className="mb-0">$499.00</h5>
                                         </div>
-                                        <div class="col-md-1 col-lg-1 col-xl-1 text-end">
-                                            <a href="#!" class="text-danger"><i class="fas fa-trash fa-lg"></i></a>
+                                        <div className="col-md-1 col-lg-1 col-xl-1 text-end">
+                                            <a href="#!" className="text-danger"><i className="fas fa-trash fa-lg"></i></a>
                                         </div>
                                     </div>
                                 </div>
                             </div>
 
-                            <div class="card rounded-3 mb-4">
-                                <div class="card-body p-4">
-                                    <div class="row d-flex justify-content-between align-items-center">
-                                        <div class="col-md-2 col-lg-2 col-xl-2">
+                            <div className="card rounded-3 mb-4">
+                                <div className="card-body p-4">
+                                    <div className="row d-flex justify-content-between align-items-center">
+                                        <div className="col-md-2 col-lg-2 col-xl-2">
                                             <img
                                                 src={img}
-                                                class="img-fluid rounded-3" alt="Cotton T-shirt" />
+                                                className="img-fluid rounded-3" alt="Cotton T-shirt" />
                                         </div>
-                                        <div class="col-md-3 col-lg-3 col-xl-3">
-                                            <p class="lead fw-normal mb-2">Curso 2</p>
-                                            <p><span class="text-muted">Duración: </span>18  </p>
+                                        <div className="col-md-3 col-lg-3 col-xl-3">
+                                            <p className="lead fw-normal mb-2">Curso 2</p>
+                                            <p><span className="text-muted">Duración: </span>18  </p>
                                         </div>
-                                        <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
-                                            <button class="btn btn-link px-2"
+                                        <div className="col-md-3 col-lg-3 col-xl-2 d-flex">
+                                            <button className="btn btn-link px-2"
                                                 onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
-                                                <i class="fas fa-minus"></i>
+                                                <i className="fas fa-minus"></i>
                                             </button>
 
                                             <input id="form1" min="0" name="quantity" value="1" type="number"
-                                                class="form-control form-control-sm" />
+                                                className="form-control form-control-sm" />
 
-                                            <button class="btn btn-link px-2"
+                                            <button className="btn btn-link px-2"
                                                 onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
-                                                <i class="fas fa-plus"></i>
+                                                <i className="fas fa-plus"></i>
                                             </button>
                                         </div>
-                                        <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                                            <h5 class="mb-0">$499.00</h5>
+                                        <div className="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
+                                            <h5 className="mb-0">$499.00</h5>
                                         </div>
-                                        <div class="col-md-1 col-lg-1 col-xl-1 text-end">
-                                            <a href="#!" class="text-danger"><i class="fas fa-trash fa-lg"></i></a>
+                                        <div className="col-md-1 col-lg-1 col-xl-1 text-end">
+                                            <a href="#!" className="text-danger"><i className="fas fa-trash fa-lg"></i></a>
                                         </div>
                                     </div>
                                 </div>
                             </div>
 
-                            <div class="card rounded-3 mb-4">
-                                <div class="card-body p-4">
-                                    <div class="row d-flex justify-content-between align-items-center">
-                                        <div class="col-md-2 col-lg-2 col-xl-2">
+                            <div className="card rounded-3 mb-4">
+                                <div className="card-body p-4">
+                                    <div className="row d-flex justify-content-between align-items-center">
+                                        <div className="col-md-2 col-lg-2 col-xl-2">
                                             <img
                                                 src={img}
-                                                class="img-fluid rounded-3" alt="Cotton T-shirt" />
-
+                                                className="img-fluid rounded-3" alt="Cotton T-shirt" />
                                         </div>
-                                        <div class="col-md-3 col-lg-3 col-xl-3">
-                                            <p class="lead fw-normal mb-2">Curso 3</p>
-                                            <p><span class="text-muted">Duración: </span>18 </p>
+                                        <div className="col-md-3 col-lg-3 col-xl-3">
+                                            <p className="lead fw-normal mb-2">Curso 2</p>
+                                            <p><span className="text-muted">Duración: </span>18  </p>
                                         </div>
-                                        <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
-                                            <button class="btn btn-link px-2"
+                                        <div className="col-md-3 col-lg-3 col-xl-2 d-flex">
+                                            <button className="btn btn-link px-2"
                                                 onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
-                                                <i class="fas fa-minus"></i>
+                                                <i className="fas fa-minus"></i>
                                             </button>
 
                                             <input id="form1" min="0" name="quantity" value="1" type="number"
-                                                class="form-control form-control-sm" />
+                                                className="form-control form-control-sm" />
 
-                                            <button class="btn btn-link px-2"
+                                            <button className="btn btn-link px-2"
                                                 onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
-                                                <i class="fas fa-plus"></i>
+                                                <i className="fas fa-plus"></i>
                                             </button>
                                         </div>
-                                        <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                                            <h5 class="mb-0">$499.00</h5>
+                                        <div className="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
+                                            <h5 className="mb-0">$499.00</h5>
                                         </div>
-                                        <div class="col-md-1 col-lg-1 col-xl-1 text-end">
-                                            <a href="#!" class="text-danger"><i class="fas fa-trash fa-lg"></i></a>
+                                        <div className="col-md-1 col-lg-1 col-xl-1 text-end">
+                                            <a href="#!" className="text-danger"><i className="fas fa-trash fa-lg"></i></a>
                                         </div>
                                     </div>
                                 </div>
                             </div>
 
-                            <div class="card rounded-3 mb-4">
-                                <div class="card-body p-4">
-                                    <div class="row d-flex justify-content-between align-items-center">
-                                        <div class="col-md-2 col-lg-2 col-xl-2">
+                           <div className="card rounded-3 mb-4">
+                                <div className="card-body p-4">
+                                    <div className="row d-flex justify-content-between align-items-center">
+                                        <div className="col-md-2 col-lg-2 col-xl-2">
                                             <img
                                                 src={img}
-                                                class="img-fluid rounded-3" alt="Cotton T-shirt" />
+                                                className="img-fluid rounded-3" alt="Cotton T-shirt" />
                                         </div>
-                                        <div class="col-md-3 col-lg-3 col-xl-3">
-                                            <p class="lead fw-normal mb-2">Curso 4</p>
-                                            <p><span class="text-muted">Duración: </span>18 </p>
+                                        <div className="col-md-3 col-lg-3 col-xl-3">
+                                            <p className="lead fw-normal mb-2">Curso 2</p>
+                                            <p><span className="text-muted">Duración: </span>18  </p>
                                         </div>
-                                        <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
-                                            <button class="btn btn-link px-2"
+                                        <div className="col-md-3 col-lg-3 col-xl-2 d-flex">
+                                            <button className="btn btn-link px-2"
                                                 onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
-                                                <i class="fas fa-minus"></i>
+                                                <i className="fas fa-minus"></i>
                                             </button>
 
                                             <input id="form1" min="0" name="quantity" value="1" type="number"
-                                                class="form-control form-control-sm" />
+                                                className="form-control form-control-sm" />
 
-                                            <button class="btn btn-link px-2"
+                                            <button className="btn btn-link px-2"
                                                 onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
-                                                <i class="fas fa-plus"></i>
+                                                <i className="fas fa-plus"></i>
                                             </button>
                                         </div>
-                                        <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                                            <h5 class="mb-0">$499.00</h5>
+                                        <div className="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
+                                            <h5 className="mb-0">$499.00</h5>
                                         </div>
-                                        <div class="col-md-1 col-lg-1 col-xl-1 text-end">
-                                            <a href="#!" class="text-danger"><i class="fas fa-trash fa-lg"></i></a>
+                                        <div className="col-md-1 col-lg-1 col-xl-1 text-end">
+                                            <a href="#!" className="text-danger"><i className="fas fa-trash fa-lg"></i></a>
                                         </div>
                                     </div>
                                 </div>
@@ -198,9 +197,9 @@ const Cursos = () => {
 
                           
 
-                            <div class="card">
-                                <div class="card-body">
-                                    <button type="button" class="btn btn-success btn-block btn-lg">Realizar Pago</button>
+                            <div className="card">
+                                <div className="card-body">
+                                    <button type="button" className="btn btn-success btn-block btn-lg">Realizar Pago</button>
                                 </div>
                             </div>
 
