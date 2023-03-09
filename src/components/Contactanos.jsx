e2ce4351e31327002e37334a7a96898786581172
eduardo-solis <solishernandezeduardoruben@gmail.com>
2023-03-09 15:58:51 -0600

    Cambiando class por clasNema

--- src/components/Contactanos.jsx
+++ src/components/Contactanos.jsx
@@ -6,34 +6,35 @@ const Contactanos = () => {
     return (
         <div className="bg">
 
-        <div className="jumbotron text-center">
-            <section class="intro">
-                <div class="mask d-flex align-items-center h-60 ">
-                    <div class="container">
-                        <div class="row justify-content-center">
-                            <div class="col-12 col-xl-9">
-                                <div class="card-body">
-                                    <div class="card-body p-5">
-                                        <div class="row d-flex align-items-center">
-                                            <div class="col-md-6 col-xl-7">
-                                                <div class="text-center pt-md-5 pb-5 my-md-5">
-                                                <img src={Contac} style={{ width: "14rem" }} class="card-img-top" alt="..." />
-                                                </div>
+            <div className="jumbotron text-center">
+                <section className="intro">
+                    <div className="mask d-flex align-items-center h-60 ">
+                        <div className="container">
+                            <div className="row justify-content-center">
+                                <div className="col-12 col-xl-9">
+                                    <div className="card-body">
+                                        <div className="card-body p-5">
+                                            <div className="row d-flex align-items-center">
+                                                <div className="col-md-6 col-xl-7">
+                                                    <div className="text-center pt-md-5 pb-5 my-md-5">
+                                                        <img src={Contac} style={{ width: "14rem" }} className="card-img-top" alt="..." />
+                                                    </div>
 
-                                            </div>
-                                            <div class="col-md-6 col-xl-4 text-center">
-                                                <h2 class="fw-bold mb-4 pb-2 text-white">Contáctanos</h2>
-                                                <div class="form-outline mb-3">
-                                                <img src="https://cdn-icons-png.flaticon.com/512/6043/6043857.png" style={{ width: "4rem" }} class="card-img-top" alt="..." /> <br />
-                                                <a href="https://goo.gl/maps/t5AY7Z3gJovJfDjs9"  target="_blank" className='text-white' >Violetas 239 Colonia Jardines de Jerez León, Guanajuato, México.<i class="fas fa-long-arrow-alt-right"></i></a>                                                   
                                                 </div>
-                                                <div class="form-outline mb-4">
-                                                <img src="https://cdn-icons-png.flaticon.com/512/732/732200.png" style={{ width: "3rem" }} class="card-img-top" alt="..." /> <br />
-                                                <a href="mailto:hola@novatec-consultores.com?" target="_blank" className='text-white'>hola@novatec-consultores.com</a>
-                                                </div>
-                                                <div class="text-center">
-                                                <a href="http://novatec-consultores.com/contacto/ " target="_blank" className="btn btn-outline-success text-white">Ver Más</a>
+                                                <div className="col-md-6 col-xl-4 text-center">
+                                                    <h2 className="fw-bold mb-4 pb-2 text-white">Contáctanos</h2>
+                                                    <div className="form-outline mb-3">
+                                                        <img src="https://cdn-icons-png.flaticon.com/512/6043/6043857.png" style={{ width: "4rem" }} className="card-img-top" alt="..." /> <br />
+                                                        <a href="https://goo.gl/maps/t5AY7Z3gJovJfDjs9" target="_blank" className='text-white' >Violetas 239 Colonia Jardines de Jerez León, Guanajuato, México.<i className="fas fa-long-arrow-alt-right"></i></a>
+                                                    </div>
+                                                    <div className="form-outline mb-4">
+                                                        <img src="https://cdn-icons-png.flaticon.com/512/732/732200.png" style={{ width: "3rem" }} className="card-img-top" alt="..." /> <br />
+                                                        <a href="mailto:hola@novatec-consultores.com?" target="_blank" className='text-white'>hola@novatec-consultores.com</a>
+                                                    </div>
+                                                    <div className="text-center">
+                                                        <a href="http://novatec-consultores.com/contacto/ " target="_blank" className="btn btn-outline-success text-white">Ver Más</a>
 
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
@@ -42,10 +43,9 @@ const Contactanos = () => {
                             </div>
                         </div>
                     </div>
-                </div>
-            </section>
+                </section>
 
-        </div>
+            </div>
         </div>
     )
 }
